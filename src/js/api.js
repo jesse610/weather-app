6d765ce98b5ba70b41c1c4ea0f3fa0197151c0f8
Jesse <jvelas028@ucr.edu>
2023-07-07 00:30:37 -0700

    weather api functions

--- src/js/api.js
+++ src/js/api.js
@@ -1,16 +1,41 @@
-// api key
-// https://api.weatherapi.com/v1/current.json?key=7b0b90612d7c491caee32617230607&q=hayward
-// 7b0b90612d7c491caee32617230607
 
-const logger = (text) => {
-    console.log(text)
+const fetchWeatherData = async (location) => {
+    try 
+    {
+        const promise = await fetch(`https://api.weatherapi.com/v1/current.json?key=7b0b90612d7c491caee32617230607&q=${location}`)
+        const data = await promise.json()
+        console.log(data)
+        return data
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
 }
 
-const getApiInfo = async () => {
-    const promise = await fetch('https://api.weatherapi.com/v1/current.json?key=7b0b90612d7c491caee32617230607&q=hayward')
-    console.log(promise)
+const processWeatherData = (data) => {
+    const processedWeatherData = {
+        location: data.location.name,
+        temperature: data.current.temp_f,
+        condition: data.current.condition.text,
+        humidity: data.current.humidity,
+    }
+
+    console.log(processedWeatherData)
+}
+
+const logWeatherData = async (location) => {
+    try {
+        const data = await fetchWeatherData(location)
+        const processedData = processWeatherData(data)
+        console.log(processedData)
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
 }
 
 export {
-    logger
+    logWeatherData
 }
