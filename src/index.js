8ad974d34cd85b10754f62b1f134d0ec5a287090
Jesse <jvelas028@ucr.edu>
2023-07-07 00:30:21 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-import { logger } from "./js/api"
+import { logWeatherData } from "./js/api"
+import { addLocationFormEventListener } from "./js/domLogic"
 
-logger('hello')
-
-const getApiInfo = async () => {
-    try 
-    {
-        const promise = await fetch('https://api.weatherapi.com/v1/current.json?key=7b0b90612d7c491caee32617230607&q=San Francisco')
-        const data = await promise.json()
-        console.log(data)
-    }
-    catch(err)
-    {
-        console.log(err)
-    }
-}
-
-getApiInfo()
+logWeatherData('San Francisco')
+addLocationFormEventListener()
