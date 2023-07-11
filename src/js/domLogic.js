844c3a087b2de775095b33340fab66193f0a3e9d
Jesse <jvelas028@ucr.edu>
2023-07-11 12:21:01 -0700

    change degree from F to C or vice versa

--- src/js/domLogic.js
+++ src/js/domLogic.js
@@ -1,5 +1,5 @@
 import { getProcessedForecastData, getWeatherData } from "./api"
-import { getAnimationCue, processedWeatherConditionCodes, translateDateToWeekday } from "./applicationLogic"
+import { getAnimationCue, translateDateToWeekday } from "./applicationLogic"
 
 const weatherDiv = document.querySelector('div')
 const locationInput = document.querySelector('#location')
@@ -10,11 +10,15 @@ const regionSpan = document.querySelector('#region')
 const countrySpan = document.querySelector('#country')
 const conditionSpan = document.querySelector('#condition')
 const tempSpan = document.querySelector('#temp')
+const tempCSpan = document.querySelector('#tempC')
 const humiditySpan = document.querySelector('#humidity')
 const windSpan = document.querySelector('#wind-speed')
+const windSpanKph = document.querySelector('#wind-speedKph')
 const feelsLikeSpan = document.querySelector('#feels-like-temp')
-const weatherAnimiationContainer = document.querySelector('.weather-animation-container')
+const feelsLikeSpanC = document.querySelector('#feels-like-tempC')
 const animationImg = document.querySelector('.weather-animation-container > img')
+const cBtn = document.querySelector('#C')
+const fBtn = document.querySelector('#F')
 const errorDiv = document.querySelector('.error')
 
 const addLocationFormEventListener = () => {
@@ -22,7 +26,6 @@ const addLocationFormEventListener = () => {
         e.preventDefault()
         let locationName = locationInput.value
         displayWeatherData(locationName)
-        // displayWeatherAnimation(locationName)
         createWeekdayDisplay(locationName)
         form.reset()
     })
@@ -44,12 +47,15 @@ const displayWeatherData = async (location) => {
             regionSpan.textContent = ''
             countrySpan.textContent = weatherDataObj.country
         }
-        
+
         conditionSpan.textContent = `${weatherDataObj.condition}`
         tempSpan.textContent = `${weatherDataObj.temperature}°F`
+        tempCSpan.textContent = `${weatherDataObj.temperatureC}°C`
         humiditySpan.textContent = `${weatherDataObj.humidity}%`
         windSpan.textContent = `${weatherDataObj.wind} mph`
+        windSpanKph.textContent = `${weatherDataObj.windK} kph`
         feelsLikeSpan.textContent = `${weatherDataObj.feelsLikeTemp}°F`
+        feelsLikeSpanC.textContent = `${weatherDataObj.feelsLikeTempC}°C`
         displayWeatherAnimation(location, weatherDataObj)
     }
     catch(err)
@@ -94,16 +100,73 @@ const createWeekdayForecast = (div, weekday, forecast, weatherAnimation) => {
     let span1 = document.createElement('span')
     span1.textContent = `H:${forecast.maxtempF}°F`
     span1.className = 'Htemp'
+    span1.classList.add('f-forecast')
     div.appendChild(span1)
 
     let span2 = document.createElement('span')
     span2.textContent = `L:${forecast.mintempF}°F`
     span2.className = 'Ltemp'
+    span2.classList.add('f-forecast')
     div.appendChild(span2)
+
+    let span3 = document.createElement('span')
+    span3.textContent = `H:${forecast.maxtempC}°C`
+    span3.className = 'Htemp'
+    span3.classList.add('hidden')
+    span3.classList.add('c-forecast')
+    div.appendChild(span3)
+
+    let span4 = document.createElement('span')
+    span4.textContent = `L:${forecast.mintempC}°C`
+    span4.className = 'Ltemp'
+    span4.classList.add('hidden')
+    span4.classList.add('c-forecast')
+    div.appendChild(span4)
 }
 
-displayWeatherData('Union City')
-createWeekdayDisplay('Union City')
+const addTempButtonEventListeners = () => {
+    cBtn.addEventListener('click', displayCTemps)
+    fBtn.addEventListener('click', displayFTemps)
+}
+
+const displayCTemps = () => {
+    fBtn.classList.remove('selected')
+    cBtn.classList.add('selected')
+
+    tempCSpan.classList.remove('hidden')
+    tempSpan.classList.add('hidden')
+
+    feelsLikeSpanC.classList.remove('hidden')
+    feelsLikeSpan.classList.add('hidden')
+
+    windSpanKph.classList.remove('hidden')
+    windSpan.classList.add('hidden')
+
+    document.querySelectorAll('.c-forecast').forEach(c => c.classList.remove('hidden'))
+    document.querySelectorAll('.f-forecast').forEach(f => f.classList.add('hidden'))
+}
+
+const displayFTemps = () => {
+    fBtn.classList.add('selected')
+    cBtn.classList.remove('selected')
+
+    tempSpan.classList.remove('hidden')
+    tempCSpan.classList.add('hidden')
+
+    feelsLikeSpan.classList.remove('hidden')
+    feelsLikeSpanC.classList.add('hidden')
+
+    windSpan.classList.remove('hidden')
+    windSpanKph.classList.add('hidden')
+
+    document.querySelectorAll('.f-forecast').forEach(f => f.classList.remove('hidden'))
+    document.querySelectorAll('.c-forecast').forEach(c => c.classList.add('hidden'))
+}
+
+addTempButtonEventListeners()
+fBtn.classList.add('selected')
+displayWeatherData('San Francisco')
+createWeekdayDisplay('San Francisco')
 
 export {
     addLocationFormEventListener
