99d6415b2bdd4afbcf7e01efd7c7d36a10bd35cc
Jesse <jvelas028@ucr.edu>
2023-07-11 00:02:54 -0700

    weather codes

--- src/js/applicationLogic.js
+++ src/js/applicationLogic.js
@@ -1,9 +1,11 @@
-import { getWeatherData } from "./api"
 
 const weatherConditionCodes = (day) => {
     const simplifiedCodes = [
         {
             clouds: [1003, 1006, 1009]
+        },
+        {
+            fog: [1030, 1135, 1147]
         },
         {
             rain: [1063, 1153, 1168, 1171, 1180, 1183, 1186, 1189, 1192, 1195, 1198, 1201, 1240, 1243, 1246]
@@ -46,13 +48,9 @@ const getWeatherConditionCodes = (isDay) => {
 }
 
 const getAnimationCue = (location, dataObj) => {
-    console.log(dataObj)
     const code = dataObj.conditionCode
     const isDay = dataObj.isDay
     const processedWeathercodes = getWeatherConditionCodes(isDay);
-
-    console.log(processedWeathercodes)
-    console.log(isDay)
 
     for (let i = 0; i < processedWeathercodes.length; i++)
     {
